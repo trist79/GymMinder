2d342124b8db776a81ffdb761525f945a33d1e35
Robert Woods <robertwoods2013@gmail.com>
2017-03-14 18:59:02 -0400

    moved error checking Workout constructor to its own constructor to avoid firebase issues <8^)

--- app/src/main/java/edu/temple/gymminder/Workout.java
+++ app/src/main/java/edu/temple/gymminder/Workout.java
@@ -4,6 +4,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,27 +15,30 @@ import java.util.List;
 public class Workout implements Serializable {
     ArrayList<Exercise> exercises;
 
-    public Workout(){
+    public Workout() {
 
     }
 
-    public Workout(ArrayList<Exercise> exercises){
-        for(Exercise e : exercises){
-            if(e.workout.length() > 50 || e.reps <=0 || e.sets <=0) exercises.remove(e);
+    public Workout(ArrayList<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
+    public Workout(ArrayList<Exercise> exercises, boolean errorCheck) {
+        for (Exercise e : exercises) {
+            if (e.workout.length() > 50 || e.reps <= 0 || e.sets <= 0) exercises.remove(e);
         }
         this.exercises = exercises;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String res = "";
-        for(Exercise e : exercises){
-            res+= e.workout +": " + e.reps + "x" + e.sets+ "\n";
+        for (Exercise e : exercises) {
+            res += e.workout + ": " + e.reps + "x" + e.sets + "\n";
         }
-        res = res.substring(0, res.length()-1);
+        res = res.substring(0, res.length() - 1);
         return res;
     }
-
 
 
 }
