d57a9d8ca6614453946da8b13ce9d8659b977679
Tristan Anderson <trist79@gmail.com>
2017-04-26 12:26:39 -0400

    Added workout history fragment support

---
+++ app/src/main/java/edu/temple/gymminder/HistoryFragment.java
@@ -0,0 +1,106 @@
+package edu.temple.gymminder;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link HistoryFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class HistoryFragment extends Fragment implements DbHelper.Listener {
+
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    DbHelper db = DbHelper.newInstance(this);
+    WorkoutsFragment.DetailListener listener;
+    private OnFragmentInteractionListener mListener;
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void updateUi(Workout workout) {
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        db.getHistory(auth.getCurrentUser());
+    }
+
+    @Override
+    public void respondToWorkouts(final ArrayList<Workout> workouts, final ArrayList<String> names) {
+        ListView lv = (ListView) getView().findViewById(R.id.historylist);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, names);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                listener.goToWorkoutsDetail(workouts.get(position), (String) parent.getAdapter().getItem(position));
+                //listener.goToWorkoutHistoryDay();
+                
+            }
+        });
+    }
+
+    @Override
+    public void onWorkoutAdded() {
+
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+    }
+}
